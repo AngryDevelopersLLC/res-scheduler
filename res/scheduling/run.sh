0b386db8d0694de1a8da6b75eadefa86de1a8bd3
Vadim Markovtsev <gmarkhor@gmail.com>
2015-11-11 17:25:54 +0300

    Remove invalid opt from run.sh
    Change-Id: I8d806e176d9ba7c0fb7262c5020d3e1d69c34f62

--- res/scheduling/run.sh
+++ res/scheduling/run.sh
@@ -3,7 +3,4 @@
 if [ -z "$SETTINGS" ]; then
   echo "SETTINGS was not specified"
 fi
-if [ -z "$PROCESSES" ]; then
-  PROCESSES=1
-fi
-python3 -m res.scheduling -vvv -c $SETTINGS -p $PROCESSES $EXTRA
+python3 -m res.scheduling -vvv -c $SETTINGS $EXTRA
